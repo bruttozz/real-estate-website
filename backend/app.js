d64b36c13070c1d43b717bd8c3275e203a844429
brutto <hongtaoz@bu.edu>
2019-10-05 23:38:58 -0400

    Added MondoDB

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const PostModel = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://brutto:<password>@bruttocluster-p4qft.mongodb.net/admin?retryWrites=true&w=majority");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -9,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    "Access-Control-Allow-Header",
+    "Access-Control-Allow-Headers",
     "Origin, X-Requested_With, Content-Type, Accept");
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -17,8 +22,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("./api/posts", (req, res, next) => {
-  const post = req.body;
+app.post("api/posts", (req, res, next) => {
+  const post = new PostModel({
+    title: req.body.title,
+    content: req.body.content
+  });
 
   console.log(post);
   res.status(201).json({message: "Post added successfully"});
