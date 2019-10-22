c3495503de0195aa6dae2769fd765e459c517ae7
brutto <hongtaoz@bu.edu>
2019-10-21 23:58:26 -0400

    add mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,52 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const mongo = require('mongodb');
 
 const PostModel = require('./models/post');
 
 const app = express();
 
-mongoose.connect("mongodb+srv://brutto:<password>@bruttocluster-p4qft.mongodb.net/admin?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://test:lclc0610@bruttocluster-p4qft.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch(() => {
+    console.log('Connection Failed')
+  });
+
+// mongoose.connection.once('open', function() {
+//   logger.info('MongoDB event open');
+//   logger.debug('MongoDB connected [%s]', url);
+//
+//   mongoose.connection.on('connected', function() {
+//     logger.info('MongoDB event connected');
+//   });
+//
+//   mongoose.connection.on('disconnected', function() {
+//     logger.warn('MongoDB event disconnected');
+//   });
+//
+//   mongoose.connection.on('reconnected', function() {
+//     logger.info('MongoDB event reconnected');
+//   });
+//
+//   mongoose.connection.on('error', function(err) {
+//     logger.error('MongoDB event error: ' + err);
+//   });
+//
+//   // return resolve();
+//   return server.start();
+// });
+//
+// mongoose.connect("mongodb+srv://test:lclc0610@bruttocluster-p4qft.mongodb.net/admin?retryWrites=true&w=majority",
+//   { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
+//   if (err) {
+//     logger.error('MongoDB connection error: ' + err);
+//     // return reject(err);
+//     process.exit(1);
+//   }
+// });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -27,16 +67,21 @@ app.post("api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
+  post.save();
 
   console.log(post);
   res.status(201).json({message: "Post added successfully"});
 });
 
-app.use("/api/posts", (req, res, next) => {
+app.get("/api/posts", (req, res, next) => {
   const posts = [
     {id: "01", title: "one", content: "oneone"},
     {id: "02", title: "two", content: "twotwo"}
   ];
+  PostModel.find().then(documents => {
+    console.log(documents);
+  });
+
   res.status(200).json({
     message: "Posts fetched successfully",
     posts: posts
